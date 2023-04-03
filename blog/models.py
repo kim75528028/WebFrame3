fc2a4b03a3c2fe305969968b472745a41d34d121
kim <kim75528028@gmail.com>
2023-04-03 15:29:38 +0900

    Post 모델 생성 및 시간 조정

--- blog/models.py
+++ blog/models.py
@@ -6,4 +6,9 @@ class Post(models.Model): #일종의 DB구조,field 설정
     title = models.CharField(max_length=30)
     content = models.TextField()
 
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    #author
+
+    def __str__(self):
+        return f'[{self.pk}] {self.title}'
