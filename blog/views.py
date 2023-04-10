db452e91711115fee1879fe8d96c794d90837f53
kim <kim75528028@gmail.com>
2023-04-10 15:40:40 +0900

    웹 페이지 생성

--- blog/views.py
+++ blog/views.py
@@ -3,16 +3,19 @@ from .models import Post
 from django.views.generic import ListView, DetailView
 
 # Create your views here.
-
-def index(request):
-    posts = Post.objects.all().order_by('pk')
-
-    return render(
-        request, 'blog/index.html',
-    {
-        'posts': posts,
-    }
-)
+class PostList(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    ordering = '-pk'
+# def index(request):
+#     posts = Post.objects.all().order_by('pk')
+#
+#     return render(
+#         request, 'blog/index.html',
+#     {
+#         'posts': posts,
+#     }
+# )
 
 def single_post_pages(request,pk):
     post = Post.objects.get(pk=pk)
