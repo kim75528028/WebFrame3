5d29e4b271e9aef764ddc07f49f21dc9341efff8
kim <kim75528028@gmail.com>
2023-05-08 15:05:18 +0900

    웹 프레임워크

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,17 @@ import os
 from django.contrib.auth.models import User
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
+
+
 class Post(models.Model): #일종의 DB구조,field 설정
     title = models.CharField(max_length=30)
     hook_text = models.CharField(max_length=100, blank=True)
