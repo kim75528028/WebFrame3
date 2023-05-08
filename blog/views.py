7077a3b7cd816bae73b2f1748d42fe9528c3bd7c
kim <kim75528028@gmail.com>
2023-05-08 14:24:03 +0900

    웹 프레임워크

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+'''
 from django.shortcuts import render
 from .models import Post
 from django.views.generic import ListView, DetailView
@@ -30,3 +31,37 @@ class PostDetail(DetailView):
 #             'post': post,
 #         }
 #     )
+'''
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+# Create your views here.
+
+class PostList(ListView):
+    model = Post
+    #template_name = 'blog/post_list.html'
+    ordering = '-pk'
+
+#def index(request):
+#    posts = Post.objects.all().order_by('-pk')
+#
+#    return render(
+#        request, 'blog/post_list.html',
+#        {
+#            'posts': posts,
+#        }
+#    )
+
+class PostDetail(DetailView):
+    model = Post
+
+#def single_post_page(request, pk):
+#    post = Post.objects.get(pk=pk)
+#    return render(
+#        request,
+#        'blog/post_detail.html',
+#        {
+#            'post': post,
+#        }
+#    )
--- blog_week9/models.py
+++ blog_week9/models.py
@@ -1,19 +1,22 @@
 from django.db import models
 import os
 from django.contrib.auth.models import User
-# Create your models here.
 
-class Post(models.Model): #일종의 DB구조,field 설정
+# Create your models here.
+class Post(models.Model):
     title = models.CharField(max_length=30)
     hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
 
-    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d', blank=True)
-    file_upload = models.FileField(upload_to='blog/images/%Y/%m/%d', blank=True)
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
-    # author
+    updated_at = models.DateTimeField(auto_now=True)
+
+    author = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+
+    # on_delete = models.SET_NULL
 
     def __str__(self):
         return f'[{self.pk}] {self.title}'
@@ -26,3 +29,4 @@ class Post(models.Model): #일종의 DB구조,field 설정
 
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1]
+
