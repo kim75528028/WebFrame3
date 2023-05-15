cac626d28cbe5fb702d099816d2f6c37f9f679d9
kim <kim75528028@gmail.com>
2023-05-15 14:28:29 +0900

    웹 프레임워크

--- blog/views.py
+++ blog/views.py
@@ -34,16 +34,24 @@ class PostDetail(DetailView):
 '''
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Post
+from .models import Post, Category
+
 
 # Create your views here.
 
 class PostList(ListView):
     model = Post
-    #template_name = 'blog/post_list.html'
+    # template_name = 'blog/post_list.html'
     ordering = '-pk'
 
-#def index(request):
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data()  # post_list
+        context['categories'] = Category.object.all()
+        context['no_category_post_count'] = Post.object, filter(category=None).count()
+        return context  # post_list.html
+
+
+# def index(request):
 #    posts = Post.objects.all().order_by('-pk')
 #
 #    return render(
@@ -56,7 +64,13 @@ class PostList(ListView):
 class PostDetail(DetailView):
     model = Post
 
-#def single_post_page(request, pk):
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data()  # post_list
+        context['categories'] = Category.object.all()
+        context['no_category_post_count'] = Post.object, filter(category=None).count()
+        return context  # post_list.html
+
+# def single_post_page(request, pk):
 #    post = Post.objects.get(pk=pk)
 #    return render(
 #        request,
--- blog_week9/views.py
+++ blog_week9/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Post
+from .models import Post,Category
 
 # Create your views here.
 
@@ -8,6 +8,13 @@ class PostList(ListView):
     model = Post
     #template_name = 'blog/post_list.html'
     ordering = '-pk'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data() # post_list
+        context['categories'] = Category.object.all()
+        context['no_category_post_count'] = Post.object,filter(category=None).count()
+        return context # post_list.html
+
 
 #def index(request):
 #    posts = Post.objects.all().order_by('-pk')
@@ -22,6 +29,12 @@ class PostList(ListView):
 class PostDetail(DetailView):
     model = Post
 
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data()  # post_list
+        context['categories'] = Category.object.all()
+        context['no_category_post_count'] = Post.object, filter(category=None).count()
+        return context  # post_detail.html (post, categories,
+
 #def single_post_page(request, pk):
 #    post = Post.objects.get(pk=pk)
 #    return render(
@@ -31,3 +44,22 @@ class PostDetail(DetailView):
 #            'post': post,
 #        }
 #    )
+
+def category_page(request, slug):
+    if slug == 'no_category':
+        category = '미분류'
+        post_list = Post.object.filter(category=None)
+    else:
+        category = Category.object.get(slug=slug)
+        post_list = Post.object.filter(category=category)
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list' : post_list,
+            'categories' : Category.object.all(),
+            'no_category_psot_count' : Post.object.filter(category=None).count(),
+            'category' : category,
+        }
+    )
